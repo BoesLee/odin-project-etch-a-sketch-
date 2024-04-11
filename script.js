24520263f0716608929b65f9c264b435779ddc07
BoesLee <stevebouchery@gmail.com>
2024-04-11 23:29:02 +0200

    fix: correct issues with grid alignment/breaks on mobile devices

--- script.js
+++ script.js
@@ -5,13 +5,41 @@ const darkeningEffect = Math.round((255/10));
 let gridSize = 16;
 buildGrid(gridSize);
 
+sizeButton.addEventListener('click', getGridSize)
+function getGridSize(){
+    sketchGrid.replaceChildren();
+    gridSize = prompt("SIZE", 16);
+    if( !Number(gridSize) || gridSize > 100 || gridSize < 1) {
+        getGridSize();
+    }
+    buildGrid(gridSize);
+}
+
+function getScreenOrientation() {
+    return screen
+      .orientation
+      .type
+      .startsWith("portrait") ? "portrait" : "landscape";
+  }
+  console.log(getScreenOrientation())
+
 function addSquare() {
-    let blockSize = (99.9 / gridSize); // not 100% for overflow reasons
-    let ratio = (window.innerWidth / window.innerHeight)
+    let blockSize = (87.6/ gridSize); // not 100% for overflow reasons
     let square = document.createElement("div");
+    let ratio;
     // counts as "...a new grid should be generated in the same total space as before..."? right? right!
-    square.style.width = `${(blockSize / ratio) * 0.85}vw`;
-    square.style.height = `${blockSize * 0.85}vh`;
+    if (getScreenOrientation() == "portrait") {
+        ratio = (screen.width / screen.height)
+        console.log(ratio)
+        square.style.height = `${(blockSize * ratio) * 0.855}vh`;
+        square.style.aspectRatio = 1 / 1;
+    }
+    else {
+        ratio = (screen.height / screen.width)
+        console.log(ratio)
+        square.style.width = `${(blockSize * ratio) * 0.855}vw`;
+        square.style.aspectRatio = 1 / 1;
+    }
     square.style.backgroundColor = "RGB(255, 255, 255)"
     square.classList = "square border";
     sketchGrid.appendChild(square);
@@ -30,22 +58,13 @@ function buildGrid() {
         }
         addRow();
     }
-}
-
-let elementsArray = document.querySelectorAll(".square");
-elementsArray.forEach((element) => element.addEventListener("mouseover",(event) => {
+    let elementsArray = document.querySelectorAll(".square");
+    elementsArray.forEach((element) => element.addEventListener("mouseover",(event) => {
         let color = event.target.style.backgroundColor;
         let rgb = color.match(/\d+/g);
         event.target.style.backgroundColor = `rgb(${rgb[0]-darkeningEffect}, ${rgb[1]-darkeningEffect}, ${rgb[2]-darkeningEffect})`;
     }
-));
-
-sizeButton.addEventListener('click', getGridSize)
-function getGridSize(){
-    sketchGrid.replaceChildren();
-    gridSize = prompt("SIZE", 16);
-    if( !Number(gridSize) || gridSize > 100 || gridSize < 1) {
-        getGridSize();
-    }
-    buildGrid(gridSize);
+    ));
 }
+
+
