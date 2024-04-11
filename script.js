1ac7bc542a7ce110c70c737c9dc14150994a851e
BoesLee <stevebouchery@gmail.com>
2024-04-11 20:02:40 +0200

    feat: add resize button

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const sketchGrid = document.getElementById("sketchGrid");
+const sizeButton = document.getElementById("sizeButton");
 const darkeningEffect = Math.round((255/10));
 
-let gridSize = 64; 
-let blockSize = (99.9 / gridSize); // not 100% for overflow reasons
-let ratio = window.innerWidth / window.innerHeight
+let gridSize = 16;
+buildGrid(gridSize);
 
 function addSquare() {
+    let blockSize = (99.9 / gridSize); // not 100% for overflow reasons
+    let ratio = (window.innerWidth / window.innerHeight)
     let square = document.createElement("div");
     // counts as "...a new grid should be generated in the same total space as before..."? right? right!
-    square.style.width = `${blockSize /ratio}vw`;
-    square.style.height = `${blockSize}vh`;
+    square.style.width = `${(blockSize / ratio) * 0.85}vw`;
+    square.style.height = `${blockSize * 0.85}vh`;
     square.style.backgroundColor = "RGB(255, 255, 255)"
     square.classList = "square border";
     sketchGrid.appendChild(square);
@@ -21,11 +23,13 @@ function addRow() {
     sketchGrid.appendChild(row);
 }
 
-for( let i = 0; i < gridSize; i++) {
-    for( let ii = 0; ii < gridSize; ii++) {
-        addSquare();
+function buildGrid() {
+    for( let i = 0; i < gridSize; i++) {
+        for( let ii = 0; ii < gridSize; ii++) {
+            addSquare();
+        }
+        addRow();
     }
-    addRow();
 }
 
 let elementsArray = document.querySelectorAll(".square");
@@ -34,4 +38,14 @@ elementsArray.forEach((element) => element.addEventListener("mouseover",(event) 
         let rgb = color.match(/\d+/g);
         event.target.style.backgroundColor = `rgb(${rgb[0]-darkeningEffect}, ${rgb[1]-darkeningEffect}, ${rgb[2]-darkeningEffect})`;
     }
-  ));
+));
+
+sizeButton.addEventListener('click', getGridSize)
+function getGridSize(){
+    sketchGrid.replaceChildren();
+    gridSize = prompt("SIZE", 16);
+    if( !Number(gridSize) || gridSize > 100 || gridSize < 1) {
+        getGridSize();
+    }
+    buildGrid(gridSize);
+}
