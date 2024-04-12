8e22bf0fcd7105dfadcb7f8104283d695bc22a82
BoesLee <stevebouchery@gmail.com>
2024-04-12 18:56:13 +0200

    feat: add random colors effect

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Odin Project: Etch-a-Sketch
 Foundations course
 
-# MAIN 5 / 5, OPTIONAL 1 / 2
+# MAIN 5 / 5, OPTIONAL 2 / 2
 Per the [assignment](https://www.theodinproject.com/lessons/foundations-etch-a-sketch#assignment)
--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 const sketchGrid = document.getElementById("sketchGrid");
 const sizeButton = document.getElementById("sizeButton");
+const colorSwitch = document.getElementById("colorSwitch");
 const darkeningEffect = Math.round((255/10));
 
-let gridSize = 16;
-buildGrid(gridSize);
+let gridSize = 64;
 
-sizeButton.addEventListener('click', getGridSize)
 function getGridSize(){
     sketchGrid.replaceChildren();
-    gridSize = prompt("SIZE", 16);
+    gridSize = prompt("Choose any number between 1 and 100", 64);
     if( !Number(gridSize) || gridSize > 100 || gridSize < 1) {
         getGridSize();
     }
     buildGrid(gridSize);
 }
 
-function getScreenOrientation() {
-    return screen
-      .orientation
-      .type
-      .startsWith("portrait") ? "portrait" : "landscape";
-  }
-  console.log(getScreenOrientation())
-
 function addSquare() {
-    let blockSize = (87.6/ gridSize); // not 100% for overflow reasons
+    let blockSize = (86/ gridSize); // not 100% for "overflow" reasons
     let square = document.createElement("div");
     let ratio;
     // counts as "...a new grid should be generated in the same total space as before..."? right? right!
+    function getScreenOrientation() {
+        return screen
+          .orientation
+          .type
+          .startsWith("portrait") ? "portrait" : "landscape";
+      }
     if (getScreenOrientation() == "portrait") {
         ratio = (screen.width / screen.height)
-        console.log(ratio)
-        square.style.height = `${(blockSize * ratio) * 0.855}vh`;
+        square.style.height = `${(blockSize * ratio) * 0.86}vh`;
         square.style.aspectRatio = 1 / 1;
     }
     else {
         ratio = (screen.height / screen.width)
-        console.log(ratio)
-        square.style.width = `${(blockSize * ratio) * 0.855}vw`;
+        square.style.width = `${(blockSize * ratio) * 0.86}vw`;
         square.style.aspectRatio = 1 / 1;
     }
     square.style.backgroundColor = "RGB(255, 255, 255)"
-    square.classList = "square border";
+    square.classList = "square gridBorder";
     sketchGrid.appendChild(square);
 }
 
@@ -51,6 +46,33 @@ function addRow() {
     sketchGrid.appendChild(row);
 }
 
+function drawColor() {
+    let elementsArray = document.querySelectorAll(".square");
+    elementsArray.forEach((element) => element.addEventListener("mouseover",(event) => {
+        if (colorSwitch.checked) {
+            function randomNumber() {
+                const min = 0;
+                const max = 255;
+                // A Proper Random Function, according to https://www.w3schools.com/JS/js_random.asp.
+                let randomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
+                return randomNumber
+            }
+            let color = event.target.style.backgroundColor;
+            console.log(color)
+            let rgb = color.match(/\d+/g);
+            event.target.style.backgroundColor = `rgb(${rgb[0] = randomNumber()}, ${rgb[1] = randomNumber()}, ${rgb[2] = randomNumber()})`;
+            console.log('Checked');
+        } 
+        else {
+            let color = event.target.style.backgroundColor;
+            console.log(color)
+            let rgb = color.match(/\d+/g);
+            event.target.style.backgroundColor = `rgb(${rgb[0] - darkeningEffect}, ${rgb[1] - darkeningEffect}, ${rgb[2] - darkeningEffect})`;
+            console.log('Not checked');
+        }
+    }));
+}
+
 function buildGrid() {
     for( let i = 0; i < gridSize; i++) {
         for( let ii = 0; ii < gridSize; ii++) {
@@ -58,13 +80,12 @@ function buildGrid() {
         }
         addRow();
     }
-    let elementsArray = document.querySelectorAll(".square");
-    elementsArray.forEach((element) => element.addEventListener("mouseover",(event) => {
-        let color = event.target.style.backgroundColor;
-        let rgb = color.match(/\d+/g);
-        event.target.style.backgroundColor = `rgb(${rgb[0]-darkeningEffect}, ${rgb[1]-darkeningEffect}, ${rgb[2]-darkeningEffect})`;
-    }
-    ));
+    drawColor()
 }
 
-
+function main() {
+    buildGrid(gridSize);
+    colorSwitch.addEventListener('change', drawColor)
+    sizeButton.addEventListener('click', getGridSize)
+}
+main()
